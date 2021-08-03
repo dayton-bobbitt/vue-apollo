f36133445f94a195a1e1e33d918236330f31048c
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2021-08-03 15:36:55 -0500

    wip(hackernews-node): create CRUD operations for posts

--- packages/hackernews-node/src/index.js
+++ packages/hackernews-node/src/index.js
@@ -1,19 +1,53 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const { ApolloServer } = require('apollo-server');
 
-const typeDefs = gql`
-    type Query {
-        info: String!
-    }
-`;
+const posts = [{
+    id: 'post-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}];
+
+let postId = posts.length;
 
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews clone`,
+        feed: () => posts,
+        post: (_parent, args) => posts.find(post => post.id === args.id),
+    },
+    Mutation: {
+        createPost: (_parent, args) => {
+            const post = {
+                id: `post-${postId++}`,
+                url: args.url,
+                description: args.description,
+            };
+
+            posts.push(post);
+            return post;
+        },
+        updatePost: (_parent, args) => {
+            const post = posts.find(post => post.id === args.id);
+
+            if (args.url) {
+                post.url = args.url;
+            }
+
+            if (args.description) {
+                post.description = args.description;
+            }
+
+            return post;
+        },
+        deletePost: (_parent, args) => {
+            const postIndex = posts.findIndex(post => post.id === args.id);
+            posts.splice(postIndex, 1);
+        },
     },
 };
 
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: readFileSync(join(__dirname, 'schema.graphql'), 'utf8'),
     resolvers,
 });
 
