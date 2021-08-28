6e829de3d98a15b8b4d2af7430aeff1b742fc709
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2021-08-28 14:06:48 -0500

    chore(hackernews-node): use apollo-server-express to enable use of subscriptions

--- packages/hackernews-node/src/index.js
+++ packages/hackernews-node/src/index.js
@@ -1,6 +1,10 @@
 const { readFileSync } = require("fs");
 const { join } = require("path");
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require("apollo-server-express");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const express = require("express");
+const http = require("http");
+
 const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
 const User = require("./resolvers/User");
@@ -13,14 +17,40 @@ const resolvers = {
   Post,
 };
 
-const server = new ApolloServer({
-  typeDefs: readFileSync(join(__dirname, "schema.graphql"), "utf8"),
-  resolvers,
-  context: ({ req }) => {
-    return {
-      req,
-    };
-  },
-});
+/**
+ * Start ApolloServer using `apollo-server-express` in order to enable
+ * subscriptions and allow us to provide define our own express middleware
+ * 
+ * @see https://www.apollographql.com/docs/apollo-server/integrations/middleware/#swapping-out-apollo-server
+ */
+async function startApolloServer() {
 
-server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const apolloServer = new ApolloServer({
+    typeDefs: readFileSync(join(__dirname, "schema.graphql"), "utf8"),
+    resolvers,
+    context: ({ req }) => {
+      return {
+        req,
+      };
+    },
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+    ],
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({
+    app,
+    path: "/"
+  });
+
+  const port = 4000;
+  await new Promise(resolve => httpServer.listen({ port }, resolve));
+  console.log(`Server is running on http://localhost:${port}${apolloServer.graphqlPath}`);
+}
+
+startApolloServer();
